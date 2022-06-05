7a18bcc702d5f43473a38f2ffba1ece8bbc67a7e
maXveGaDeveloper <maximiliano.jesus.vega@gmail.com>
2022-06-05 14:35:49 -0300

    updates with hugo

--- proyectoStereo/settings.py
+++ proyectoStereo/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'proyectoStereo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'plantillas')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- proyectoStereo/urls.py
+++ proyectoStereo/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from proyectoStereo.views import saludo, probandoTemplate
+from .views import saludo, probandoTemplate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
