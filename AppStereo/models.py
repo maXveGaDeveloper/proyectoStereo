b5e15ac102e5fb5a4c520f15152e7bfa269fbd6e
maXveGaDeveloper <maximiliano.jesus.vega@gmail.com>
2022-06-05 21:28:58 -0300

    update: modelos y DB

--- AppStereo/models.py
+++ AppStereo/models.py
@@ -6,10 +6,16 @@ class Musicos(models.Model):
     apellido = models.CharField(max_length=50)
     edad = models.IntegerField()
     instrumento = models.CharField(max_length=50)
+    banda = models.CharField(max_length=50)
 
 class Instrumentos(models.Model):
     nombre = models.CharField(max_length=50)
     modelo = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=50)
 
-        
+class albums(models.Model):
+    nombre = models.CharField(max_length=50)
+    artista = models.CharField(max_length=50)
+    fecha_lanzamiento = models.DateField()
+    genero = models.CharField(max_length=50)
+     
