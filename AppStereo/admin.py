d456dc117c2b93e668f9bccc87a73135f0729390
maXveGaDeveloper <maximiliano.jesus.vega@gmail.com>
2022-06-07 12:43:26 -0300

    creacion del admin y el superuser

--- AppStereo/admin.py
+++ AppStereo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Musicos)
+admin.site.register(Instrumentos)
+admin.site.register(albums)
